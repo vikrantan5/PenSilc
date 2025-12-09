72d9f0a98e82041d6f958c26207db1a3d6d701ed
Vikrant Singh <vikrantsinghan5@gmail.com>
2025-12-09 17:15:09 +0530

    small changes

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -124,7 +124,7 @@ export function Landing() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <div className="text-center text-gray-600">
             <p>&copy; 2025 PenSilc. All rights reserved .</p>
-            <p>&copy; Made with ❤️ by Vikrant Singh</p>
+            <p>Made with ❤️ by Vikrant Singh</p>
           </div>
         </div>
       </footer>
